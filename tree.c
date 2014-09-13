31f4fe466ef3f8cc4e62b169d2732df3041a5a26
Aayush Chhabra <chhabraaayush@yahoo.ca>
2014-09-13 18:15:46 -0400

    Working with Trees:)

--- tree.c
+++ tree.c
@@ -9,6 +9,63 @@ struct node{
 }*root;
 
 
+struct treeNode{
+	int number;
+	struct treeNode* next;
+}*front, *rear;
+
+int isEmpty(){
+	if(front == NULL){
+		return 1;
+	}
+	return 0;
+}
+
+void push(int num){
+	struct treeNode* temp = (struct treeNode*)malloc(sizeof(struct treeNode));
+	temp->number = num;
+	temp->next = NULL;
+
+
+	if((front == NULL)&&(rear == NULL)){
+		front = rear = temp;
+		return;
+	}
+
+	rear->next = temp;
+	rear = rear->next;
+}
+
+struct treeNode* pop(){
+	struct treeNode * temp;
+	if(front==NULL){
+		printf("Nuthing to pop..\n");
+		return NULL;
+	}
+
+	if(front == rear)
+	{
+		temp = front;
+		front = rear = NULL;
+		return temp;
+	}
+
+	temp = front;
+	front = front -> next;
+	return temp;
+
+}
+
+
+void print(){
+	struct treeNode* temp = front;
+	while(temp != rear){
+		printf("%d\n", temp->number);
+		temp = temp->next;
+	}
+	printf("%d\n", temp->number);
+}
+
 
 // int ListCreate(struct node* root, int array[100], int level ){
 // 	if(root == NULL)
@@ -44,11 +101,11 @@ void addNode(struct node ** root, int number){
 	}
 }
 
-void print(struct node* root){
+void print1(struct node* root){
 	if(root!=NULL){
-		print(root->left);
+		print1(root->left);
 		printf("%d\n",root->number);
-		print(root->right);
+		print1(root->right);
 	}
 }
 
@@ -108,11 +165,153 @@ void deleteNode(struct node** root, int number){
 }
 
 
+struct node* searchNode(struct node* root, int number){
+	
+	if(root == NULL)
+		return NULL;
+
+	if(root->number == number){
+		return root;
+	}
+
+	if(root->number < number){
+		return searchNode(root->right, number);
+	}
+
+	if(root->number > number){
+		return searchNode(root->left, number);
+	}
+}
+
+int searchInVisited(int un[10], int number){
+	for(int i=0; un[i]!=-1; i++){
+		if(un[i] == number){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void addToUn(struct node* root, int un[10], int level){
+	
+	if(root == NULL)
+		return;
+	
+	while(un[level] != -1){
+		level ++;
+	}
+
+	un[level] = root->number;
+
+	addToUn(root->left, un, level);
+	addToUn(root->right, un, level);
+}
+
+
+int bfs(struct node* root, struct treeNode* front, struct treeNode* rear,int visited[10],int un[10]){
+	int i;
+	if(!isEmpty()){
+		//printf("Iteration: ");print();
+		struct treeNode* temp = pop();
+		
+		if(!searchInVisited(visited, temp->number)){
+			for(i=0; visited[i]!=-1; i++);
+			visited[i] = temp->number;	
+		}
+
+		struct node * temp1 = searchNode(root, temp->number);
+
+		if(temp1->left)
+			push((temp1->left)->number);
+		if(temp1->right)
+		push((temp1->right)->number);
+
+		bfs(root, front, rear, visited, un);
+		bfs(root, front, rear, visited, un);
+
+	}
+	else
+		return 0;
+}
+
+
+int heightMax(struct node* root){
+	
+	if(root==NULL)
+		return 0;
+
+	int lheight = 0, rheight = 0;
+	lheight = heightMax(root->left);
+	rheight = heightMax(root->right);
+
+
+	if(lheight>rheight)
+		return(lheight+1);
+
+	else
+		return(rheight+1);
+
+
+
+}
+
+// void heightMax(struct node * root, int *max, int count){
+	
+// 	if(root == NULL)
+// 		return;
+
+// 	count++;
+
+// 	if((root->left == NULL) && (root->right == NULL)){
+// 		if(*max < count)
+// 			*max = count;
+// 		return;
+// 	}
+
+// 	//printf("%d : %d\n", root->number, count);
+
+// 	heightMax(root->left, max, count);
+// 	heightMax(root->right, max, count);
+// }
+
+
+// void heightMin(struct node * root, int *max, int count){
+	
+
+
+// 	if(root == NULL)
+// 		return;
+
+
+// 	count ++;
+	
+// 	if((root->left == NULL) && (root->right == NULL)){
+// 		if(*max > count)
+// 			*max = count;
+// 		return;
+// 	}
+
+
+// 	heightMin(root->left, max, count);
+// 	heightMin(root->right, max, count);
+
+// }
+
+
+
+
+
 int main(){
-	// int array[100];
-	// memset(array, -1, 400);
-	// for(int i=0; i<100; i++)
-	// 	printf("%d\n",array[i]);
+	// // int array[100];
+	// // memset(array, -1, 400);
+	// // for(int i=0; i<100; i++)
+	// // 	printf("%d\n",array[i]);
+	
+
+	// front = rear = NULL;
+	// int visited[10], un[10], length = 0;
+	// memset(visited, -1, 40);
+	// memset(un, -1, 40);
 	root = NULL;
 	addNode(&root, 10);
 	addNode(&root, 3);
@@ -123,10 +322,31 @@ int main(){
 	addNode(&root, 17);
 	addNode(&root, 32);
 
-	print(root);
+	//print1(root);
 
-	// ListCreate(root, array, 0);
-	// for(int i=0; i<10; i++)
-	// 	printf("%d\n",array[i]);
+
+	// int max = 0, min=100;
+	int z = heightMax(root);
+	printf("%d\n", z);
+	// heightMin(root, &min, -1);
+
+	// printf("%d\n",max - min);
+
+
+	//addToUn(root, un, 0);
+
+	// // for(int i =0; un[i]!=-1; i++){
+	// // 	printf("here I am: %d\n", un[i]);
+	// // }
+
+	// push(root->number);
+	// bfs(root, front, rear, visited, un);
+
+	// for(int i=0; visited[i]!=-1; i++){
+	// 	printf("%d\n", visited[i]);
+	// }
+	// // ListCreate(root, array, 0);
+	// // for(int i=0; i<10; i++)
+	// // 	printf("%d\n",array[i]);
 
 }
--- treeB.c
+++ treeB.c
@@ -1,50 +1,59 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-struct node{
+struct treeNode{
 	int number;
-	struct node* next;
-}*root;
+	struct treeNode* next;
+}*front, *rear;
 
-
-struct linkedList{
-	struct node value;
-};
-
-
-int lengthOfQ = 0;
 
 void push(int num){
-	struct node* temp = (struct node*)malloc(sizeof(struct node));
+	struct treeNode* temp = (struct treeNode*)malloc(sizeof(struct treeNode));
 	temp->number = num;
 	temp->next = NULL;
 
-	if(lengthOfQ == 9)
-		printf("POP\n");
 
-	else{
-		array[lengthOfQ] = temp;
-		lengthOfQ++;
+	if((front == NULL)&&(rear == NULL)){
+		front = rear = temp;
+		return;
 	}
+
+	rear->next = temp;
+	rear = rear->next;
 }
 
-struct node* pop(){
-	struct node* temp = array[0];
-	for(int i=1; i<lengthOfQ; i++){
-		array[i-1] = array[i];
-		lengthOfQ--;
+struct treeNode* pop(){
+	struct treeNode * temp;
+	if(front==NULL){
+		printf("Nuthing to pop..\n");
+		return NULL;
 	}
+
+	if(front == rear)
+	{
+		temp = front;
+		front = rear = NULL;
+		return temp;
+	}
+
+	temp = front;
+	front = front -> next;
 	return temp;
+
 }
 
 
 void print(){
-	for(int i=0; i<lengthOfQ; i++){
-		printf("%d\n", array[i]->number);
+	struct treeNode* temp = front;
+	while(temp != rear){
+		printf("%d\n", temp->number);
+		temp = temp->next;
 	}
+	//printf("%d\n", temp->number);
 }
 
 int main(){
+	front = rear = NULL;
 	push(10);
 	push(20);
 	push(30);
@@ -52,10 +61,16 @@ int main(){
 	push(50);
 	push(60);
 	//print();
-	struct node* temp = pop();
-	pop();
-	pop();
-	pop();
-	pop();
-	print();
+	struct treeNode* temp = pop();
+	printf("%d\n",temp->number);
+	struct treeNode* temp1 = pop();
+	printf("%d\n",temp1->number);
+	struct treeNode* temp2 = pop();
+	printf("%d\n",temp2->number);
+	struct treeNode* temp3 = pop();
+	printf("%d\n",temp3->number);
+	struct treeNode* temp4 = pop();
+	printf("%d\n",temp4->number);
+	struct treeNode* temp5 = pop();
+	printf("%d\n",temp5->number);
 }
